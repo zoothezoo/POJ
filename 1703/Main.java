957e37b6ba1b65b85363a39d87859055aa270725
zoothezoo <kohekoshien@gmail.com>
2018-05-18 18:40:53 +0900

    new

--- 1703/Main.java
+++ 1703/Main.java
@@ -1,61 +1,75 @@
-//ID:zoothezoo1
-import java.util.ArrayList;
+//zoothezoo1
 import java.util.Scanner;
-import java.util.List;
+import java.io.*;
 
 public class Main{
-    static boolean[] mark;
-    static List<List<Integer>> criminals = new ArrayList<List<Integer>>();
-    static String answer;
-
-    static void search(int start,int goal,int step){
-        if(start == goal){
-            if(step % 2 == 0){
-                answer = "In the same gang.";
+    public static void main(String[] args)throws IOException{
+        //Scanner sc = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int test = Integer.parseInt(br.readLine());
+        for(int i=0 ; i<test ; i++){
+            String[] input = br.readLine().split(" ");
+            int n = Integer.parseInt(input[0]);
+            int q = Integer.parseInt(input[1]);
+            UF uf = new UF(n);
+            for(int j=0 ; j<q ; j++){
+                String[] in = br.readLine().split(" ");
+                String query = in[0];
+                //System.out.println("query ==" + query);
+                int a = Integer.parseInt(in[1]);
+                int b = Integer.parseInt(in[2]);
+                if(query.equals("D")){
+                    uf.marge(a*2,b*2+1);
+                    uf.marge(a*2+1,b*2);
+                }
+                else{
+                    System.out.println(uf.same(a,b));
+                }
             }
-            else{
-                answer = "In different gangs.";
-            }
-            return;
-        }
-        if(mark[start]){
-            return;
-        }
-        mark[start] = true;
-        for(int w : criminals.get(start)){
-            search(w,goal,step+1);
         }
     }
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        int tests = Integer.parseInt(sc.nextLine());
-        for(int t = 0 ; t < tests ; t++){
-            String[] line = sc.nextLine().split(" ");
-            int numbers = Integer.parseInt(line[0]);
-            mark = new boolean[numbers+1];
-            int inputs = Integer.parseInt(line[1]);
-            for(int i=0 ; i <= numbers ; i++){
-                List<Integer> neighbors = new ArrayList<Integer>();
-                criminals.add(neighbors);
+    static class UF{
+        int[] parent;
+        int[] rank;
+        public UF(int N){
+            this.parent = new int[N*2+2];
+            this.rank = new int[N*2+2];
+            for(int i=1 ; i<=N*2+1 ; i++){
+                parent[i] = -1;
+                rank[i] = 0;
             }
-            for(int s=0 ; s < inputs ; s++){
-                String[] input = sc.nextLine().split(" ");
-                char command = input[0].charAt(0);
-                int crimA = Integer.parseInt(input[1]);
-                int crimB = Integer.parseInt(input[2]);
-                if(command == 'A'){
-                    for(int i=0 ; i < mark.length ; i++){
-                        mark[i]=false;
-                    }
-                    answer = "Not sure yet.";
-                    search(crimA,crimB,0);
-                    System.out.println(answer);
-                }
-                else if(command == 'D'){
-                    criminals.get(crimA).add(crimB);
-                    criminals.get(crimB).add(crimA);
-                }
+        }
+        public void marge(int a, int b){
+            a = root(a);
+            b = root(b);
+            if(a == b){
+                return;
             }
+            if(rank[a] < rank[b])
+                parent[a] = b;
+            else
+                parent[b] = a;
+            if(rank[a] == rank[b])
+                rank[a]++;
+            return;
+        }
+
+        public int root(int a){
+            if(parent[a] == -1){
+                return a;
+            }
+            else{
+                return parent[a] = root(parent[a]);
+            }
+        }
+
+        public String same(int a, int b){
+            if(root(a*2) == root(b*2))
+                return "In the same gang.";
+            else if(root(a*2) == root(b*2+1))
+                return "In different gangs.";
+            else
+                return "Not sure yet.";
         }
     }
 }
