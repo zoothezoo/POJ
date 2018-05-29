82719fd74291a8b47ca0509ad304a8303c106a15
zoothezoo <kohekoshien@gmail.com>
2018-05-29 15:57:07 +0900

    new

--- 1721/Main.java
+++ 1721/Main.java
@@ -4,22 +4,36 @@ import java.util.ArrayList;
 
 public class Main{
 
-    List<Integer> card = new ArrayList<Integer>();
-    List<Integer> mark = new ArrayList<Integer>();
-    List<Integer> ans = new ArrayList<Integer>();
 
 
-    static void shuffle(int n){
+    public int shuffle(int n,List<Integer> card ,List<Integer> mark,List<Integer> ans){
         int length = 0;
-        while(1){
+        while(true){
             for(int i=1;i<=n;i++){
-                mark.set
+                mark.set(i,card.get(card.get(i)));
+            }
+            length++;
+            boolean flag = true;
+            for(int i=1; i<=n;i++){
+                if(mark.get(i) != ans.get(i)){
+                    flag = false;
+                    break;
+                }
+            }
+            if(flag){
+                break;
+            }
+            for(int i=1 ; i<=n ; i++){
+                card.set(i,mark.get(i));
             }
         }
-
+        return length;
     }
 
     public static void main(String[] args){
+        List<Integer> card = new ArrayList<Integer>();
+        List<Integer> mark = new ArrayList<Integer>();
+        List<Integer> ans = new ArrayList<Integer>();
         Scanner sc = new Scanner(System.in);
         String[] input = sc.nextLine().split(" ");
         int n = Integer.parseInt(input[0]);
@@ -30,7 +44,16 @@ public class Main{
             ans.add(num);
         }
         int length;
-        length = shuffle(n);
-        System.out.println(card);
+        length = shuffle(n,card ,mark, ans);
+        s = s %length;
+        for(int i= 1 ;i<=n;i++){
+            mark.set(i,card.get(card.get(i)));
+        }
+        for(int i= 1 ;i<=n;i++){
+            card.set(i,mark.get(i));
+        }
+        for(int i=1;i<=n;i++){
+            System.out.println(card.get(i));
+        }
     }
 }
