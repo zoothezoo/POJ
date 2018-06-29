41cd4bcc96c29b32480db55b7bd19865303d361f
zoothezoo <kohekoshien@gmail.com>
2018-06-29 19:30:09 +0900

    [

---
+++ 1787/Fib.java
@@ -0,0 +1,42 @@
+
+class Fib {
+    static int[] memo = new int[10000];
+    // memo[i] fib(i)
+    public static int memoFib(int n) {
+        if(n <= 1){
+            return 1;
+        }
+
+        if(memo[n] != -1) {
+            return memo[n];
+        }
+
+        memo[n] = memoFib(n - 1) + memoFib(n - 2);
+        return memo[n];
+    }
+
+    public static int fib(int n) {
+        if(n <= 1){
+            return 1;
+        }
+
+        return fib(n - 1) + fib(n - 2);
+    }
+
+    public static void main(String[] args){
+        java.util.Arrays.fill(memo, -1);
+        int n = Integer.valueOf(args[0]);
+
+
+        long begin1 = System.currentTimeMillis();
+        System.out.println(memoFib(n));
+        long end1 = System.currentTimeMillis();
+
+        long begin2 = System.currentTimeMillis();
+        System.out.println(fib(n));
+        long end2 = System.currentTimeMillis();
+
+        System.out.println(end1 - begin1);
+        System.out.println(end2 - begin2);
+    }
+}
--- 1787/Main.java
+++ 1787/Main.java
@@ -1,15 +1,73 @@
+//zoothezoo1
 import java.util.*;
 
 public class Main{
+    static int[] coin = new int[4];
+    static int[] cost = new int[4];
+    static int[][] optS;
+    // i: コインの種類を表す添字
+    // Q: 支払総額(残りの支払額)
+    public static int opt(int i, int Q){
+        //再帰しない場合
+        if(i < 0){
+            if(Q == 0){
+                return 0;
+            }else{
+                return -1;
+            }
+        }
+        System.out.println("i: " + i + " q: " + Q);
+        //再帰する場合
+        int max = -1; //枚数:returnする
+        for(int s=0; s<=coin[i]; s++){ // s: コインの支払い枚数
+            if(Q < cost[i]*s){
+                break;
+            }
+            int o = opt(i-1,Q-cost[i]*s);
+            if(o < 0){
+                continue;
+            }
+            int v = o + s;
+            if(v >= max){
+                optS[i][Q] = s;
+                max = v;
+            }
+        }
+        return max;
+    }
+
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        String[] input = sc.nextLine().split(" ");
-        int price = Integer.parseInt(input[0]); //
-        int cent = Integer.parseInt(input[1]); //1cent
-        int nickel= Integer.parseInt(input[2]); //5cent
-        int dime = Integer.parseInt(input[3]); //10cent
-        int quarter = Integer.parseInt(input[4]); //25cent
+        while(true){
+            String[] input = sc.nextLine().split(" ");
+            int price = Integer.parseInt(input[0]);
+            for(int i=0; i<4; i++){
+                coin[i] = Integer.parseInt(input[i+1]);
+            }
+            if(price == 0 && coin[0] == 0 && coin[1] == 0 && coin[2] == 0 && coin[3] == 0){
+                return;
+            }
+            cost[0] = 1;
+            cost[1] = 5;
+            cost[2] = 10;
+            cost[3] = 25;
+            optS = new int[4][price+1];
+            int ans = opt(3,price);
+            int quarters = optS[3][price];
+            int dimes = optS[2][price - cost[3]*quarters];
+            int nickels= optS[1][price - cost[3]*quarters - cost[2]*dimes];
+            int cents= optS[0][price - cost[3]*quarters - cost[2]*dimes - cost[1]*nickels];
+            //System.out.printf("It costs %d. I have %d cents, %d nickels, %d dimes and %d quarters.\n\n",price,coin[0],coin[1],coin[2],coin[3]);
+            // System.out.println("price: "+ price);
+            // System.out.println("ans: "+ans);
+            // System.out.println("paying: "+ (cost[3]*quarters + cost[2]*dimes + cost[1]*nickels + cost[0]*cents));
+            if(ans == -1){
+                System.out.println("Charlie cannot buy coffee.");
+            }
+            else{
+                System.out.printf("Throw in %d cents, %d nickels, %d dimes, and %d quarters.\n",cents,nickels,dimes,quarters);
+            }
 
-        System.out.printf("I have %d cents, %d nickels, %d dimes and %d quarters.\nI am the richest man of the world.\n",cent,nickel,dime,quarter);
+        }
     }
 }
